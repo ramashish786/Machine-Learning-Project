89ed8cf6542ad6b1969a30e9fac907b707b932b8
Ramashish Sahani <ramashishsahani786@gmail.com>
2026-02-15 12:52:16 +0530

    Adding file

---
+++ GenAI/HR Agent/hr_data.py
@@ -0,0 +1,5 @@
+employee = {
+    1 :{"name":"Ram","age":27,"salary":49300,"manager":"2"},
+    2 :{"name":"Rohan","age":25,"salary":45300,"manager":"3"},
+    3 :{"name":"Pramod","age":35,"salary":105300,"manager":None}
+}
